2b2a9e190187b15f0b416dcd805640ab7008c8a8
horike37 <horike37@gmail.com>
2018-04-13 18:26:25 +0900

    add deploy

--- deploy/openFaasDeploy.js
+++ deploy/openFaasDeploy.js
@@ -1,5 +1,7 @@
 'use strict';
 const BbPromise = require('bluebird');
+const chalk = require('chalk');
+const normalizeUrl = require('normalize-url');
 const OpenFaasSdk = require('../lib/openFaasSdk');
 
 class OpenFaasDeploy {
@@ -7,6 +9,7 @@ class OpenFaasDeploy {
     this.serverless = serverless;
     this.options = options;
     this.provider = this.serverless.getProvider('faas');
+    this.gateway = normalizeUrl(this.serverless.service.provider.gateway);
     this.sdk = new OpenFaasSdk(this.serverless, 'func_functions');
 
     this.hooks = {
@@ -16,7 +19,27 @@ class OpenFaasDeploy {
   }
 
   deploy() {
-    return this.sdk.deploy();
+    return this.sdk.deploy()
+      .then(() => this.sdk.getDeployedFunctions())
+      .then(functions => {
+        let message = '';
+        message += `${chalk.yellow.underline('Service Information')}\n`;
+        message += `${chalk.yellow('gateway:')} ${this.serverless.service.provider.gateway}\n`;
+        message += `${chalk.yellow('endpoints:')}\n`;
+        BbPromise.each(functions, f => {
+          message += `  POST - ${this.gateway}/function/${f.name}\n`;
+        }).then(() => {
+          message += `${chalk.yellow('functions:')}\n`;
+          return BbPromise.resolve();
+        }).then(() =>
+          BbPromise.each(functions, f => {
+            message += `  ${f.name}: ${f.image}\n`;
+          })
+        ).then(() => {
+          this.serverless.cli.consoleLog(message);
+          return BbPromise.resolve();
+        });
+      });
   }
 }
 
