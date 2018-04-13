44ca32edbe540202c718eb229db22f26eb74a88a
horike37 <horike37@gmail.com>
2018-04-13 10:41:04 +0900

    add deploy

--- deploy/openFaasDeploy.js
+++ deploy/openFaasDeploy.js
@@ -1,13 +1,13 @@
 'use strict';
 const BbPromise = require('bluebird');
-const fetch = require('node-fetch');
+const OpenFaasSdk = require('../lib/openFaasSdk');
 
 class OpenFaasDeploy {
   constructor(serverless, options) {
     this.serverless = serverless;
     this.options = options;
     this.provider = this.serverless.getProvider('faas');
-    this.gateway = this.serverless.service.provider.gateway;
+    this.sdk = new OpenFaasSdk(this.serverless, 'func_functions');
 
     this.hooks = {
       'deploy:deploy': () => BbPromise.bind(this)
@@ -16,24 +16,7 @@ class OpenFaasDeploy {
   }
 
   deploy() {
-    const endpoint = `${this.serverless.service.provider.gateway}/system/functions`;
-    const allFunctions = this.serverless.service.getAllFunctions();
-    return BbPromise.each(
-      allFunctions,
-      functionName =>
-        fetch(endpoint, {
-          method: 'PUT',
-          body: JSON.stringify({
-            service: functionName,
-            image: this.serverless.service.functions[functionName].image,
-            network: 'func_functions',
-          }),
-          headers: { 'Content-Type': 'application/json' },
-        })
-        .then(res => {
-          console.log(res);
-        })
-    );
+    return this.sdk.deploy();
   }
 }
 
